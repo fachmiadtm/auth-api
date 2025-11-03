aba4a4789523146355808e2817b85c1e4dd8c7c8
Fachmi Aditama <fachmi8adtm@gmail.com>
2025-11-03 16:53:41 +0700

    add say hello world test case

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -1,40 +1,48 @@
+const pool = require('../../database/postgres/pool');
+const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const AuthenticationsTableTestHelper = require('../../../../tests/AuthenticationsTableTestHelper');
+// const injections = require('../../injections');
 const createServer = require('../createServer');
-
+ 
 describe('HTTP server', () => {
+  afterAll(async () => {
+   await pool.end();
+  });
+ 
+  afterEach(async () => {
+   await UsersTableTestHelper.cleanTable();
+   await AuthenticationsTableTestHelper.cleanTable();
+  });
+ 
   it('should response 404 when request unregistered route', async () => {
-    // Arrange
-    const server = await createServer({});
-
-    // Action
-    const response = await server.inject({
+   // Arrange
+   const server = await createServer({});
+ 
+   // Action
+   const response = await server.inject({
       method: 'GET',
       url: '/unregisteredRoute',
     });
-
-    // Assert
+ 
+   // Assert
     expect(response.statusCode).toEqual(404);
   });
-
-  it('should handle server error correctly', async () => {
-    // Arrange
-    const requestPayload = {
-      username: 'dicoding',
-      fullname: 'Dicoding Indonesia',
-      password: 'super_secret',
-    };
-    const server = await createServer({}); // fake injection
-
-    // Action
-    const response = await server.inject({
-      method: 'POST',
-      url: '/users',
-      payload: requestPayload,
+ 
+ describe('when GET /', () => {
+   it('should return 200 and hello world', async () => {
+     // Arrange
+      const server = await createServer({});
+     // Action
+      const response = await server.inject({
+        method: 'GET',
+        url: '/',
+      });
+     // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(200);
+      expect(responseJson.value).toEqual('Hello world!');
     });
-
-    // Assert
-    const responseJson = JSON.parse(response.payload);
-    expect(response.statusCode).toEqual(500);
-    expect(responseJson.status).toEqual('error');
-    expect(responseJson.message).toEqual('terjadi kegagalan pada server kami');
   });
+ 
+ // Skenario testing lain ...
 });
